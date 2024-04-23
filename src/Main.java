87d2dfc7f0c5693af388473d200241ba0d7c90fa
Adalberto R. Ribeiro <beto1821@uol.com.br>
2024-04-23 12:32:17 -0300

    metodos insert

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import br.com.alura.screenmatch.modelos.Movie;
+
 public class Main {
     public static void main(String[] args) {
-        Movie newMovie = new Movie();
+        Movie meuFilme = new Movie();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
-        newMovie.nome =  "StarWars";
-        newMovie.anoDeLançamento = 1978;
-        newMovie.duracaoEmMinutos = 180;
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.getSomaDasAvaliacoes());
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
 
-        System.out.println(newMovie.nome);
     }
 }
--- src/Movie.java
+++
@@ -1,8 +0,0 @@
-public class Movie {
-    String nome;
-    int anoDeLançamento;
-    boolean incluidoNoPlano;
-    double avaliação;
-    int totaldeAvaliacoes;
-    int duracaoEmMinutos;
-}
